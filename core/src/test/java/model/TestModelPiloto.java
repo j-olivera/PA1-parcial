5151c03673b30be29012f6b0944b3aa05a0667e1
j-olivera <juanswc87@gmail.com>
2025-10-14 17:41:27 -0300

    test model - definiciones

--- core/src/test/java/model/TestModelPiloto.java
+++ core/src/test/java/model/TestModelPiloto.java
@@ -5,6 +5,20 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class TestModelPiloto {
+    @Test
+    void testCrearPiloto(){
+
+    }
+
+    @Test
+    void testVerificarDatosObligatoriosException(){
+
+    }
+
+    @Test
+    void verificarDniYaExisteException(){
+
+    }
 
 }
 
