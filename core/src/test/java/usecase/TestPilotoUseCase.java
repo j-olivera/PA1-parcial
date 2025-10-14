8776c6c5d5e3557eb9f9181fb3eb3b77653ddedb
j-olivera <juanswc87@gmail.com>
2025-10-14 18:01:00 -0300

    testRegistrarPiloto pasa

--- core/src/test/java/usecase/TestPilotoUseCase.java
+++ core/src/test/java/usecase/TestPilotoUseCase.java
@@ -1,14 +1,41 @@
 package usecase;
 
+import model.Piloto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import output.PilotoRepository;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class TestPilotoUseCase {
+
+    @Mock
+    PilotoRepository pilotoRepository;
+    @InjectMocks
+    PilotoUseCase pilotoUseCase;
+
     @Test
     void testRegistrarPiloto() {
-        //
-
+        //Arrange
+        String nombre = "Franco Colapinto";
+        String dni = "123456ABC";
+        LocalDate fecha_nacimiento = LocalDate.of(2000, 12, 12);
+        Piloto esperado = Piloto.crearPiloto(nombre,dni,fecha_nacimiento);
+        when(pilotoRepository.save(any(Piloto.class))).thenReturn(esperado.getLicencia());
+        //Act
+        UUID resultado = pilotoUseCase.guardarPiloto(nombre,dni,fecha_nacimiento);
+        //Assert
+        Assertions.assertEquals(esperado.getLicencia(),resultado);
+        verify(pilotoRepository,times(1)).save(any(Piloto.class));
     }
     void testDniYaRegistrado(){
         //
