a520d3a15fde9ac88e3eab094b3e80bf26572992
j-olivera <juanswc87@gmail.com>
2025-10-14 17:55:39 -0300

    estructura TestUseCaseModel

---
+++ core/src/test/java/usecase/TestPilotoUseCase.java
@@ -0,0 +1,19 @@
+package usecase;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class TestPilotoUseCase {
+    @Test
+    void testRegistrarPiloto() {
+        //
+
+    }
+    void testDniYaRegistrado(){
+        //
+    }
+}
+//El caso de uso debe devolver la Licencia asignada al Piloto creado con éxito
+//No puede existir dos Pilotos con el mismo documento
+
